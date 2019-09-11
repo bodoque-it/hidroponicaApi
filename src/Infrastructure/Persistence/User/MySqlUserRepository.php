84e190aa995dfb6635847a7e8179a323d98d13a3
Francisco Antilef <francisco.antilef.jaramillo@gmail.com>
2019-09-11 10:59:05 -0300

    Agrega implementacion de mysql inicial
    la persistencia final sera en mysql

--- src/Infrastructure/Persistence/User/MySqlUserRepository.php
+++ src/Infrastructure/Persistence/User/MySqlUserRepository.php
@@ -1,10 +1,35 @@
 <?php
 
-
 namespace App\Infrastructure\Persistence\User;
 
 
-class MySqlUserRepository
+use App\Domain\User\User;
+use App\Domain\User\UserNotFoundException;
+use App\Domain\User\UserRepository;
+
+class MySqlUserRepository implements UserRepository
 {
 
+    /**
+     * @return User[]
+     */
+    public function findAll(): array
+    {
+        // TODO: Implement findAll() method.
+    }
+
+    /**
+     * @param int $id
+     * @return User
+     * @throws UserNotFoundException
+     */
+    public function findUserOfId(int $id): User
+    {
+        // TODO: Implement findUserOfId() method.
+    }
+
+    public function getPassword(int $id): string
+    {
+        // TODO: Implement getPassword() method.
+    }
 }
