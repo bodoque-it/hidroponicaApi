4021d57ce127f3a2021de74393b9e4fb147a7e3a
Francisco Antilef <francisco.antilef.jaramillo@gmail.com>
2019-09-30 06:19:32 -0300

    Registra usuario

--- src/Infrastructure/Persistence/User/MySqlUserRepository.php
+++ src/Infrastructure/Persistence/User/MySqlUserRepository.php
@@ -8,6 +8,7 @@ use App\Domain\User\UserNotFoundException;
 use App\Domain\User\UserRepository;
 use PDO;
 use phpDocumentor\Reflection\Types\Boolean;
+use PhpParser\Node\Expr\Cast\Bool_;
 use Psr\Container\ContainerInterface;
 use function DI\get;
 
@@ -53,22 +54,37 @@ class MySqlUserRepository implements UserRepository
         return  new User($id,$row["Username"],$row["first_name"],$row["last_name"],$row["Email"]);
     }
 
-    public function getPassword(int $id): string
+    public function getPassword(string $username): string
     {
-        $query = "SELECT Contraseña FROM  Usuario WHERE id_user = ?";
+        $query = "SELECT Contraseña FROM  Usuario WHERE Username = ?";
         $sth = $this->db->prepare($query);
-        $sth->bindParam(1,$id);
+        $sth->bindParam(1,$username);
         $sth->execute();
         $row = $sth->fetch();
         return $row["Contraseña"];
     }
-    public function createUser(string $username, string $first_name, string $last_name, string $password, string $email): User
+    public function createUser(string $username, string $first_name, string $last_name, string $password, string $email): bool
     {
+        if(!$this->existUserByUsername($username)){
+            $query = "INSERT INTO Usuario(Username,first_name,last_name,Contraseña,Email) VALUES (?,?,?,?,?)";
+            $sth = $this->db->prepare($query);
+            return $sth->execute([$username,$first_name,$last_name,$password,$email]);
 
-        $query = "INSERT INTO Usuario(Username,first_name,last_name,Contraseña,Email) VALUES (?,?,?,?,?)";
-        $sth = $this->db->prepare($query);
-        $sth->execute([$username,$first_name,$last_name,$password,$email]);
-        return new User(0,$username,$first_name,$last_name,$email);
+        }
+        return false;
+
+
+
     }
-    
+    public function existUserByUsername(string $username):bool{
+        $query = "SELECT * FROM Usuario WHERE Username = ?";
+        $sth = $this->db->prepare($query);
+        $sth->bindParam(1,$username);
+        $sth->execute();
+        $result = $sth->fetch();
+        if($result["Username"]!=null){
+            return true;
+        }
+        return false;
+    }
 }
