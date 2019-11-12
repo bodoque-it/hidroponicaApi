50cade752b855dcfcde96dacadab6cc5d7c94101
Francisco Antilef <francisco.antilef.jaramillo@gmail.com>
2019-11-12 19:10:54 -0300

    Recupera la api

--- app/middleware.php
+++ app/middleware.php
@@ -6,4 +6,12 @@ use Slim\App;
 
 return function (App $app) {
     $app->add(SessionMiddleware::class);
+    $app->add(new Tuupola\Middleware\CorsMiddleware([
+        "origin" => ["*"],
+        "methods" => ["GET", "POST", "PUT", "PATCH", "DELETE"],
+        "headers.allow" => ["Authorization", "If-Match", "If-Unmodified-Since"],
+        "headers.expose" => ["Etag"],
+        "credentials" => true,
+        "cache" => 86400
+    ]));
 };
