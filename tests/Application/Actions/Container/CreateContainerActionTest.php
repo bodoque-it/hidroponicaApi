e6a21c0934f3329e2fa85ccdf871ac317b487f0d
Francisco Antilef <francisco.antilef.jaramillo@gmail.com>
2020-03-24 14:07:59 -0300

    Enable auth

--- tests/Application/Actions/Container/CreateContainerActionTest.php
+++ tests/Application/Actions/Container/CreateContainerActionTest.php
@@ -10,10 +10,10 @@ use Tests\TestCase;
 class CreateContainerActionTest extends TestCase
 {
     public function testAction(){
-//        $app = $this->getAppInstance();
-//        $json = json_encode(array("name"=>"name","volume"=>10.0));
-//        $request = $this->createRequest('POST','api/containers/1');
-//        $response = $app->handle($request);
+        $app = $this->getAppInstance();
+        $request = $this->createRequest('POST','api/containers/1/1');
+        $request = $request->withParsedBody(["param1"=>"param1value"]);
+        $response = $app->handle($request);
         $this->assertEquals(200, 200);
     }
 }
